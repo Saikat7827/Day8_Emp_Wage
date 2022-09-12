50d2780ac0a2096d03b76d86da95e8d1ac166a8c
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-09-13 00:05:43 +0530

    Emp wage calculation using Methods and Variables

---
+++ Using_Methods_Variables_Empwg.java
@@ -0,0 +1,27 @@
+package com.bl.day8empwge;
+
+import java.util.Scanner;
+
+public class Using_Methods_Variables_Empwg {
+	 static int wagePerHour = 20;
+	    static int hoursPerDay = 8;
+	    void CalculateMonthlyWageBasedOnTotalDaysOrTotalHours(int userInput , int workingDays , int hoursPerMonth){
+	        if ((userInput == 1) & (workingDays <= 20))
+	            System.out.println("The Monthly Wage of Employee based on total working days=" + wagePerHour * hoursPerDay * workingDays + " Rs");
+	        else if ((userInput == 2) & (hoursPerMonth <= 100))
+	            System.out.println("The Monthly Wage of Employee based on total working hours=" + wagePerHour * hoursPerMonth + " Rs");
+	        else
+	            System.out.println("Invalid Input from User");
+	    }
+	    public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter 1 for computing in days\nEnter 2 for computing in hour");
+	        int userInput = sc.nextInt();
+	        System.out.println("Enter total number of working days");
+	        int workingDays = sc.nextInt();
+	        System.out.println("Enter total number of working hours");
+	        int hoursPerMonth = sc.nextInt();
+	        Using_Methods_Variables_Empwg wage = new Using_Methods_Variables_Empwg();
+	        wage.CalculateMonthlyWageBasedOnTotalDaysOrTotalHours(userInput , workingDays , hoursPerMonth);
+	    }
+}
