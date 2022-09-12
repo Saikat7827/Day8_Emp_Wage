95e2ff5fb2dba384f37bab486e06ddfa95ccdfdc
Saikat7827 <saikatchakraborty957.sc@gmail.com>
2022-09-12 23:24:05 +0530

    Per Day Wage

---
+++ Daily_Wage.java
@@ -0,0 +1,24 @@
+package com.bl.day8empwge;
+
+import java.util.Random;
+
+public class Daily_Wage {
+	public static void main(String[] args) {
+
+		int empHrs = 0;
+		int wageperhr = 20;
+		Random random = new Random();
+		int randomnum = random.nextInt(2);
+
+		if (randomnum == 1)
+		{
+			System.out.println("Employee is Present");
+			empHrs = 8;
+
+		}
+		else 
+			System.out.println("Employee is Absent");
+		double salary = empHrs * wageperhr;
+		System.out.println("Employee Wage is" +salary );
+	}
+}
